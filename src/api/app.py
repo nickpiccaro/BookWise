7c7c15669c6c89b64fb91adb4186f87d7cb565d4
npiccaro <npiccaro@deloitte.com>
2023-07-26 13:05:27 -0400

    heroku stuff

--- src/api/app.py
+++ src/api/app.py
@@ -6,18 +6,23 @@ This module provides functionality for XYZ.
 Author: Your Name
 """
 
-from flask import Flask
+from flask import Flask, send_from_directory
 from werkzeug.middleware.proxy_fix import ProxyFix
-from flask_cors import CORS  # Import CORS module
+from flask_cors import CORS, cross_origin  # Import CORS module
 from modules import api
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='../ui/build', static_url_path='')
 CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
 app.wsgi_app = ProxyFix(app.wsgi_app)
 
 api.init_app(app)
+
+@app.route('/')
+@cross_origin()
+def serve():
+    return send_from_directory(app.static_folder, 'index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
